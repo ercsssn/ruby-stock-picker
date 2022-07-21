79640c3762e43cd277d6a2e08956d6d93944c213
ercsssn <ercsssn@gmail.com>
2022-07-21 22:35:01 +0800

    Create initial loop logic

--- script.rb
+++ script.rb
@@ -2,7 +2,17 @@ sample_price_array = [17,3,6,9,15,8,6,10,1]
 
 def stock_picker(price_array)
     price_array.each do |price|
-        p price
+        max_profit = 0
+        puts price
+        
+        price_array.each_with_index do |current_price, day|
+            current_array = price_array[0, day + 1]
+            p current_array
+            profit = price - current_price
+            if (profit) > max_profit 
+                max_profit = profit
+            end
+        end
     end
 
 end
