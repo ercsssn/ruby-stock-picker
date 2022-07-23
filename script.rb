701c6afff982f5d0c5bade600c1bf73ef6209f4d
ercsssn <ercsssn@gmail.com>
2022-07-24 01:17:42 +0800

    Comment out entire code
    Maybe formulate a new solution using hash

--- script.rb
+++ script.rb
@@ -1,16 +1,21 @@
 sample_price_array = [17,3,6,9,15,8,6,10,1]
 
 def stock_picker(price_array)
-    price_array.each do |price|
-        max_profit = 0
-        puts price
+    # price_array.each do |price|
+    #     max_profit = 0
+    #     # puts price
 
-        price_index = price_array.find_index(price)
-        sell_price_array = price_array.drop(price_index + 1)
+    #     sell_index_array.each_index{ |i| puts i }
 
-        if !sell_price_array.empty?
-            p sell_price_array
-        end
+    #     sell_price_array = price_array.drop(price_index + 1) # add 1 to exclude the current price
+
+        # if !sell_price_array.empty?
+        #     # p sell_price_array
+        #     sell_price_array.each do |sell_price|
+        #         profit = sell_price - price
+        #         # puts profit
+        #     end
+        # end
         
         
         # price_array.each_with_index do |current_price, day|
