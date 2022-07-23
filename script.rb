a7d2b91e05074f2eb06d3c850969d30ca4413f07
ercsssn <ercsssn@gmail.com>
2022-07-23 23:52:11 +0800

    Get the remaining prices array after buying

--- script.rb
+++ script.rb
@@ -4,15 +4,21 @@ def stock_picker(price_array)
     price_array.each do |price|
         max_profit = 0
         puts price
+
+        price_index = price_array.find_index(price)
+        sell_price_array = price_array.drop(price_index + 1)
+
+        p sell_price_array
         
-        price_array.each_with_index do |current_price, day|
-            current_array = price_array[0, day + 1]
-            p current_array
-            profit = price - current_price
-            if (profit) > max_profit 
-                max_profit = profit
-            end
-        end
+        
+        # price_array.each_with_index do |current_price, day|
+        #     current_array = price_array[0, day + 1]
+        #     p current_array
+        #     profit = price - current_price
+        #     if (profit) > max_profit 
+        #         max_profit = profit
+        #     end
+        # end
     end
 
 end
